bab274917ecc3465d5c6eefce73f00f35c83c4d5
PavelKcroiz <pasikov_pavel1@mail.ru>
2020-05-23 20:24:12 +0300

    lesson6

---
+++ src/components/error-button/error-button.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./error-button.css";
+
+class ErrorButton extends React.Component {
+  state = {
+    error: false,
+  };
+
+  render() {
+    if (this.state.error) {
+      this.foo.bar = 0;
+    }
+    return (
+      <button
+        className="btn btn-danger btn-lg mb-4"
+        onClick={() => this.setState({ error: true })}
+      >
+        Trow Error
+      </button>
+    );
+  }
+}
+
+export default ErrorButton;
